fc78f5c22c9ab62afb13ea52d689a4ab70522542
Debatosh Pal Majumder <36793454+debatoshpm@users.noreply.github.com>
2021-12-01 16:30:24 +0530

    minor change

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { google } = require("googleapis");
 const path = require("path");
 const app = express();
 
@@ -9,26 +8,8 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.get("/contact/submit", async (req, res) => {
-  const auth = new google.auth.GoogleAuth({
-    keyFile: "credentials.json",
-    scopes: "https://www.googleapis.com/auth/spreadsheets",
-  });
-
-  const client = await auth.getClient();
-
-  const googleSheets = google.sheets({ version: "v4", auth: client });
-
-  const spreadsheetId = "1HCdTf2kucn7sjLWPkMgClFdc_O1YWC7xC9A9VZwYZu0";
-
-  const metaData = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId,
-  });
-
-  res.send(metaData);
-});
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App running on port${PORT}`);
+});
